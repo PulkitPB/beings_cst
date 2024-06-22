cdf42cbbfc295c9916380e38b91c80fa83fd2adf
PulkitPB <bhardwajpulkit910@gmail.com>
2024-06-22 15:13:31 +0530

    Initial commit

---
+++ backend/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+# class demo(models.Model):
+#     code=models.CharField(max_length=10,default=" ",unique=True)
+#     flag=models.BooleanField(null=False,default=False)
+#     models.IntegerField
+#     models.DateTimeField
+
